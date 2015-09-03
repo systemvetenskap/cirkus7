3797d640cfb5d9f91c7672771bc8d10d52c40acd
miby1400 <miby1400@student.miun.se>
2015-09-03 13:39:51 +0200

    test knapp

--- cirkus/Form1.Designer.cs
+++ cirkus/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(83, 93);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(131, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "En rubrik";
+            this.button1.Text = "Test Michael";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
