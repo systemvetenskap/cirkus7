a2a2f187ce85eb9a91ed619590035aad14d6e494
hnkrpnk <hkauhanen@live.se>
2015-09-10 11:13:33 +0200

    update

--- cirkus/MainForm.cs
+++ cirkus/MainForm.cs
@@ -52,7 +52,7 @@ namespace cirkus
         {
             
         }
-        private void ListaPersonal()//Metod för att lista personalen i Datagriden
+        private void ListaPersonal()//Metod för att lista personalen i Datagriden....
         {
             string sqlSearchStaff = textBoxSearchStaff.Text;
             string sql = "SELECT staffid, lname, fname, phonenumber  FROM staff WHERE fname LIKE '%" + sqlSearchStaff + "%' OR lname LIKE '%" + sqlSearchStaff + "%'";
