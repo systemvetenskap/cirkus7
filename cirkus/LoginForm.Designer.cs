39b8896cb529f220b9680953a86a757997d196fc
hnkrpnk <hkauhanen@live.se>
2015-09-14 03:07:51 +0200

    update

--- cirkus/LoginForm.Designer.cs
+++ cirkus/LoginForm.Designer.cs
@@ -28,47 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.button1 = new System.Windows.Forms.Button();
             this.textUsername = new System.Windows.Forms.TextBox();
             this.textPassword = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 81);
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(26, 358);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.Size = new System.Drawing.Size(260, 39);
             this.button1.TabIndex = 0;
             this.button1.Text = "Logga in";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textUsername
             // 
-            this.textUsername.Location = new System.Drawing.Point(12, 12);
+            this.textUsername.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textUsername.Location = new System.Drawing.Point(26, 263);
             this.textUsername.Name = "textUsername";
-            this.textUsername.Size = new System.Drawing.Size(260, 20);
+            this.textUsername.Size = new System.Drawing.Size(260, 30);
             this.textUsername.TabIndex = 1;
+            this.textUsername.Text = "Användarnamn";
+            this.textUsername.Click += new System.EventHandler(this.textUsername_Click);
+            
             // 
             // textPassword
             // 
-            this.textPassword.Location = new System.Drawing.Point(12, 38);
+            this.textPassword.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPassword.Location = new System.Drawing.Point(26, 313);
             this.textPassword.Name = "textPassword";
-            this.textPassword.Size = new System.Drawing.Size(260, 20);
+            this.textPassword.Size = new System.Drawing.Size(260, 30);
             this.textPassword.TabIndex = 2;
+            this.textPassword.Text = "lösen";
             this.textPassword.UseSystemPasswordChar = true;
+            this.textPassword.Click += new System.EventHandler(this.textPassword_Click);
+         
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 212);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // LoginForm
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 116);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(332, 418);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textPassword);
             this.Controls.Add(this.textUsername);
             this.Controls.Add(this.button1);
             this.Name = "LoginForm";
             this.Text = "Logga in";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +104,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textUsername;
         private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
