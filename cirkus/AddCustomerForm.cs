0fd1f482087b37e8518fbd78f6f109cad7cbbb3b
hnkrpnk <hkauhanen@live.se>
2015-09-15 10:24:03 +0200

    giltig email

--- cirkus/AddCustomerForm.cs
+++ cirkus/AddCustomerForm.cs
@@ -27,6 +27,13 @@ namespace cirkus
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string s = textBoxEmail.Text;
+            if (IsValidEmail(s) == false)
+            {
+                MessageBox.Show("Ange en giltig mailadress");
+                return;
+            }
+
             NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432; User Id=pgmvaru_g7;Password=akrobatik;Database=pgmvaru_g7;SSL=true;");
             try
             {
@@ -47,6 +54,18 @@ namespace cirkus
             {
                 MessageBox.Show(ex.ToString());
                 conn.Close();
+            }
+        }
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
             }
         }
     }
