24d7d79bff88e605b763f9e3f78740106785d255
miby1400 <miby1400@student.miun.se>
2015-09-29 21:56:00 +0200

    ui

--- cirkus/ChangeTicketForm.Designer.cs
+++ cirkus/ChangeTicketForm.Designer.cs
@@ -33,7 +33,6 @@
             this.dgSelectedCustomerTicket = new System.Windows.Forms.DataGridView();
             this.btnSave = new System.Windows.Forms.Button();
             this.lblAnge = new System.Windows.Forms.Label();
-            this.lblTo = new System.Windows.Forms.Label();
             this.btnChangeTicket = new System.Windows.Forms.Button();
             this.lblTodaysDate = new System.Windows.Forms.Label();
             this.checkBoxPaidTicket = new System.Windows.Forms.CheckBox();
@@ -44,18 +43,18 @@
             // 
             this.dtpTicketTo.Enabled = false;
             this.dtpTicketTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpTicketTo.Location = new System.Drawing.Point(28, 110);
+            this.dtpTicketTo.Location = new System.Drawing.Point(17, 61);
             this.dtpTicketTo.Name = "dtpTicketTo";
-            this.dtpTicketTo.Size = new System.Drawing.Size(164, 20);
+            this.dtpTicketTo.Size = new System.Drawing.Size(226, 20);
             this.dtpTicketTo.TabIndex = 0;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 21);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 17);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(200, 20);
+            this.label3.Size = new System.Drawing.Size(159, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "Ändra biljett reservation";
             // 
@@ -69,7 +68,7 @@
             this.dgSelectedCustomerTicket.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dgSelectedCustomerTicket.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgSelectedCustomerTicket.GridColor = System.Drawing.SystemColors.Window;
-            this.dgSelectedCustomerTicket.Location = new System.Drawing.Point(28, 136);
+            this.dgSelectedCustomerTicket.Location = new System.Drawing.Point(17, 87);
             this.dgSelectedCustomerTicket.MultiSelect = false;
             this.dgSelectedCustomerTicket.Name = "dgSelectedCustomerTicket";
             this.dgSelectedCustomerTicket.ReadOnly = true;
@@ -82,7 +81,7 @@
             // btnSave
             // 
             this.btnSave.Enabled = false;
-            this.btnSave.Location = new System.Drawing.Point(148, 244);
+            this.btnSave.Location = new System.Drawing.Point(137, 183);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(106, 23);
             this.btnSave.TabIndex = 9;
@@ -93,24 +92,15 @@
             // lblAnge
             // 
             this.lblAnge.AutoSize = true;
-            this.lblAnge.Location = new System.Drawing.Point(28, 66);
+            this.lblAnge.Location = new System.Drawing.Point(17, 46);
             this.lblAnge.Name = "lblAnge";
             this.lblAnge.Size = new System.Drawing.Size(226, 13);
             this.lblAnge.TabIndex = 10;
             this.lblAnge.Text = "Ange hur länge biljetten ska kunna reserveras:";
             // 
-            // lblTo
-            // 
-            this.lblTo.AutoSize = true;
-            this.lblTo.Location = new System.Drawing.Point(28, 95);
-            this.lblTo.Name = "lblTo";
-            this.lblTo.Size = new System.Drawing.Size(67, 13);
-            this.lblTo.TabIndex = 12;
-            this.lblTo.Text = "Till och med:";
-            // 
             // btnChangeTicket
             // 
-            this.btnChangeTicket.Location = new System.Drawing.Point(28, 244);
+            this.btnChangeTicket.Location = new System.Drawing.Point(17, 183);
             this.btnChangeTicket.Name = "btnChangeTicket";
             this.btnChangeTicket.Size = new System.Drawing.Size(106, 23);
             this.btnChangeTicket.TabIndex = 13;
@@ -121,7 +111,7 @@
             // lblTodaysDate
             // 
             this.lblTodaysDate.AutoSize = true;
-            this.lblTodaysDate.Location = new System.Drawing.Point(208, 110);
+            this.lblTodaysDate.Location = new System.Drawing.Point(289, 69);
             this.lblTodaysDate.Name = "lblTodaysDate";
             this.lblTodaysDate.Size = new System.Drawing.Size(79, 13);
             this.lblTodaysDate.TabIndex = 14;
@@ -131,7 +121,7 @@
             // 
             this.checkBoxPaidTicket.AutoSize = true;
             this.checkBoxPaidTicket.Enabled = false;
-            this.checkBoxPaidTicket.Location = new System.Drawing.Point(284, 250);
+            this.checkBoxPaidTicket.Location = new System.Drawing.Point(273, 189);
             this.checkBoxPaidTicket.Name = "checkBoxPaidTicket";
             this.checkBoxPaidTicket.Size = new System.Drawing.Size(107, 17);
             this.checkBoxPaidTicket.TabIndex = 15;
@@ -143,11 +133,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 279);
+            this.ClientSize = new System.Drawing.Size(860, 219);
             this.Controls.Add(this.checkBoxPaidTicket);
             this.Controls.Add(this.lblTodaysDate);
             this.Controls.Add(this.btnChangeTicket);
-            this.Controls.Add(this.lblTo);
             this.Controls.Add(this.lblAnge);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dgSelectedCustomerTicket);
@@ -170,7 +159,6 @@
         private System.Windows.Forms.DataGridView dgSelectedCustomerTicket;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Label lblAnge;
-        private System.Windows.Forms.Label lblTo;
         private System.Windows.Forms.Button btnChangeTicket;
         private System.Windows.Forms.Label lblTodaysDate;
         private System.Windows.Forms.CheckBox checkBoxPaidTicket;
