8c9e7ff2e227bd7b18c36eb8a82e3d9dbaf87a43
miby1400 <miby1400@student.miun.se>
2015-09-29 11:24:52 +0200

    Login password recovery.

---
+++ cirkus/PasswordRecoveryForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace cirkus
+{
+    partial class PasswordRecoveryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSendPassword = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(20, 252);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(260, 20);
+            this.textBoxEmail.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(20, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ange din epost adress:";
+            // 
+            // btnSendPassword
+            // 
+            this.btnSendPassword.Location = new System.Drawing.Point(20, 290);
+            this.btnSendPassword.Name = "btnSendPassword";
+            this.btnSendPassword.Size = new System.Drawing.Size(260, 30);
+            this.btnSendPassword.TabIndex = 2;
+            this.btnSendPassword.Text = "Skicka lösenord";
+            this.btnSendPassword.UseVisualStyleBackColor = true;
+            this.btnSendPassword.Click += new System.EventHandler(this.btnSendPassword_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::cirkus.Properties.Resources.cirkus_kul_bus_300x211;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 212);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Cyan;
+            this.lblStatus.Location = new System.Drawing.Point(20, 334);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(45, 15);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "status";
+            this.lblStatus.Visible = false;
+            // 
+            // PasswordRecoveryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(310, 356);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSendPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEmail);
+            this.Name = "PasswordRecoveryForm";
+            this.Text = "PasswordRecoveryForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ cirkus/PasswordRecoveryForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using System.Net;
+using System.Web;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace cirkus
+{
+    public partial class PasswordRecoveryForm : Form
+    {
+        public PasswordRecoveryForm()
+        {
+            InitializeComponent();
+        }
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432; User Id=pgmvaru_g7;Password=akrobatik;Database=pgmvaru_g7;SSL=true;");
+
+        private void btnSendPassword_Click(object sender, EventArgs e)
+        {
+            string email = textBoxEmail.Text;
+            string firstname;
+            string lastname;
+            string password;
+            string username;
+
+            lblStatus.Visible = false;
+
+            if (!IsValidEmail(email))
+            {
+                lblStatus.Visible = true;
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Vänligen ange en giltig e-post adress";
+                return;
+            }
+            else
+            {
+                try
+                {
+                    conn.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand(@"select fname, lname, email, password, username FROM staff WHERE email='"+email+"'", conn);
+                    NpgsqlDataReader dr = cmd.ExecuteReader();
+                    dr.Read();
+
+                    firstname = dr[0].ToString();
+                    lastname = dr[1].ToString();
+                    email = dr[2].ToString();
+                    password = dr[3].ToString();
+                    username = dr[4].ToString();
+                    
+
+                    string password_mail ="Hej "+firstname+" "+lastname+"\nDitt lösenord är: "+password+"\nDitt användarnamn är: "+username+" ";
+
+                    conn.Close();
+
+                    MailMessage mail = new MailMessage("kulbusstest@gmail.com", email, "Cirkus Kul&Bus - Lösenord", password_mail);
+
+                    SmtpClient client = new SmtpClient("smtp.gmail.com");
+                    client.Port = 587;
+                    client.Credentials = new System.Net.NetworkCredential("kulbusstest@gmail.com", "Test12345");
+                    client.EnableSsl = true;
+
+                    client.Send(mail);
+
+                    lblStatus.Visible = true;
+                    lblStatus.Text = "Lösenordet har skickats till din e-post";
+                    return;
+                }
+                catch (NpgsqlException)
+                {
+                    lblStatus.Visible = true;
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "E-posten finns inte registrerad hos oss";
+                    return;
+                }
+            }
+        }
+    }
+}
